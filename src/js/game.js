ad7cd516926c6b72c4995b1771b3d232783c5c3b
Joe Sorensen <Joseph.md.Sorensen@icloud.com>
2019-01-28 10:23:29 -0700

    Complete code overhaul to allow for level tilemaps.
    Added tileset and tilemap data

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,8 @@
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
-    }
-  },
+const config = {
+  type: Phaser.AUTO, // Which renderer to use
+  width: window.innerWidth, // Canvas width in pixels
+  height: window.innerHeight, // Canvas height in pixels
+  parent: "game-container", // ID of the DOM element to add the canvas to
   scene: {
     preload: preload,
     create: create,
@@ -16,171 +10,51 @@ var config = {
   }
 };
 
-var player;
-var stars;
-var bombs;
-var platforms;
-var cursors;
-var score = 0;
-var gameOver = false;
-var scoreText;
+const game = new Phaser.Game(config);
+let controls;
 
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+function preload() {
+  this.load.image("level-tiles", "assets/simples_pimples.png");
+  this.load.tilemapTiledJSON("map", "assets/BeowulfDev1.json");
 }
 
-function create ()
-{
-  //  A simple background for our game
-  this.add.image(400, 300, 'sky');
+function create() {
+  const map = this.make.tilemap({ key: "map" });
 
-  //  The platforms group contains the ground and the 2 ledges we can jump on
-  platforms = this.physics.add.staticGroup();
+  const tileset = map.addTilesetImage("16x16", "level-tiles");
 
-  //  Here we create the ground.
-  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  const BG = map.createStaticLayer("Background", tileset, 0, 0);
+  const Ground = map.createStaticLayer("Ground", tileset, 0, 0);
 
-  //  Now let's create some ledges
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+  // Phaser supports multiple cameras, but you can access the default camera like this:
+  const camera = this.cameras.main;
 
-  // The player and its settings
-  player = this.physics.add.sprite(100, 450, 'dude');
-
-  //  Player physics properties. Give the little guy a slight bounce.
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  //  Our player animations, turning, walking left and walking right.
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
+  // Set up the arrows to control the camera
+  const cursors = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left: cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5
   });
 
-  this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
-  });
+  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  //  Input Events
-  cursors = this.input.keyboard.createCursorKeys();
-
-  //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 }
-  });
-
-  stars.children.iterate(function (child) {
-
-    //  Give each star a slightly different bounce
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-  });
-
-  bombs = this.physics.add.group();
-
-  //  The score
-  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-  //  Collide the player and the stars with the platforms
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
-  this.physics.add.collider(bombs, platforms);
-
-  //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  this.physics.add.overlap(player, stars, collectStar, null, this);
-
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
+  // Help text that has a "fixed" position on the screen
+  this.add
+    .text(16, 16, "Arrow keys to scroll", {
+      font: "18px monospace",
+      fill: "#ffffff",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#000000"
+    })
+    .setScrollFactor(0);
 }
 
-function update ()
-{
-  if (gameOver)
-  {
-    return;
-  }
-
-  if (cursors.left.isDown)
-  {
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-  }
-  else if (cursors.right.isDown)
-  {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  }
-  else
-  {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down)
-  {
-    player.setVelocityY(-330);
-  }
+function update(time, delta) {
+  controls.update(delta);
 }
 
-function collectStar (player, star)
-{
-  star.disableBody(true, true);
-
-  //  Add and update the score
-  score += 10;
-  scoreText.setText('Score: ' + score);
-
-  if (stars.countActive(true) === 0)
-  {
-    //  A new batch of stars to collect
-    stars.children.iterate(function (child) {
-
-      child.enableBody(true, child.x, 0, true, true);
-
-    });
-
-    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-    var bomb = bombs.create(x, 16, 'bomb');
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    bomb.allowGravity = false;
-
-  }
-}
-
-function hitBomb (player, bomb)
-{
-  this.physics.pause();
-
-  player.setTint(0xff0000);
-
-  player.anims.play('turn');
-
-  gameOver = true;
-}

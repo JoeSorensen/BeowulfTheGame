2caae69c2bc9430442a1c1771f3d85e5708e3852
Mikerific <mikerific2@gmail.com>
2019-02-04 19:00:34 -0700

    Try to fix the frame issue and probably fail again

--- src/js/game.js
+++ src/js/game.js
@@ -76,7 +76,7 @@ function create() {
 
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('beowulf', {start: 5, end: 8}),
+    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
     frameRate: 10,
     repeat: -1
   });
